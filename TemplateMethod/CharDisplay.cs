62902cf75195445d866579665a393d3d086e9baf
正晃 <lion0024.xyz@gmail.com>
2021-01-16 19:40:30 +0900

    TemplateMethodパターンの実装

---
+++ TemplateMethod/CharDisplay.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace GoFDesignPattern.TemplateMethod
+{
+    public class CharDisplay : AbstractDisplay
+    {
+        private char ch;
+        public CharDisplay(char ch)
+        {
+            this.ch = ch;
+        }
+
+        public override void Open()
+        {
+            Console.Write("<<");
+        }
+
+        public override void Print()
+        {
+            Console.Write(ch);
+        }
+
+        public override void Close()
+        {
+            Console.WriteLine(">>");
+        }
+    }
+}
---
+++ TemplateMethod/StringDisplay.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace GoFDesignPattern.TemplateMethod
+{
+    public class StringDisplay : AbstractDisplay
+    {
+        private string str;
+        private int width;
+        public StringDisplay(string str)
+        {
+            this.str = str;
+            Encoding UniEnc = Encoding.GetEncoding("UTF-8");
+            width = UniEnc.GetByteCount(str);
+        }
+
+        public override void Open()
+        {
+            PrintLine();
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("|" + str + "|");
+        }
+
+        public override void Close()
+        {
+            PrintLine();
+        }
+
+        private void PrintLine()
+        {
+            Console.Write("+");
+            for (int i = 0; i < width; i++)
+            {
+                Console.Write("-");
+            }
+            Console.WriteLine("+");
+        }
+    }
+}
