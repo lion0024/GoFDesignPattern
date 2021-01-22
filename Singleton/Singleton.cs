6934a1a8bc1b40e6e387c41b5b6de26c53086e21
正晃 <lion0024.xyz@gmail.com>
2021-01-22 19:37:45 +0900

    Singletonパターンの実装

---
+++ Singleton/Singleton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GoFDesignPattern.Singleton
+{
+    public class Singleton
+    {
+        private static Singleton singleton = new Singleton();
+        private Singleton()
+        {
+            Console.WriteLine("インスタンスを生成しました。");
+        }
+
+        public static Singleton GetInstance()
+        {
+            return singleton;
+        }
+    }
+}
