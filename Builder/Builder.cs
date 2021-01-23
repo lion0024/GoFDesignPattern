8e8eabcce7743d071b38a3d33ec95f7e29881c86
正晃 <lion0024.xyz@gmail.com>
2021-01-23 22:00:42 +0900

    Builderの空クラス

---
+++ Builder/Builder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GoFDesignPattern.Builder
+{
+    public abstract class Builder
+    {
+        public abstract void MakeTitle(string title);
+        public abstract void MakeString(string str);
+        public abstract void MakeItems(string[] items);
+        public abstract void Close();
+    }
+}
